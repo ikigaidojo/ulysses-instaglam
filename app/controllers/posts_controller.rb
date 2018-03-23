f77db8528ae644bed3be9afcde7405ba9b92e6b8
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2018-03-23 17:04:19 +0800

    Upload is now working.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:caption)
+      params.require(:post).permit(:caption, :posted_image)
     end
 end
